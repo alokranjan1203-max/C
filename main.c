f0d0281a6a974dbaf48a1bdb5d904034cdcd90bb
alokranjan1203-max <alokranjan1203@gmail.com>
2025-09-09 19:33:43 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc,char* argv[]){
+char* portAsString=getenv("PORT");
+if(portAsString == NULL){
+    portAsString="3000";
+
+}
+int port = atoi(portAsString);
+printf("THe port number is %i\n",port);
+    return 0;
+}
